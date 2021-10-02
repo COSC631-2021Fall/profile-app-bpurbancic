f23e6ea9b40c4e27de9761e26a1448574f6a04d1
Brian <burbanci@emich.edu>
2021-10-02 23:45:07 +0000

    First react app

---
+++ src/Hobbies.js
@@ -0,0 +1,17 @@
+const Hobbies = () => {
+    return ( 
+        <div>
+            <h3>
+                I'm new here, so here goes.
+                
+            </h3>
+            <h4>
+            When I'm not coding, I enjoy long walks on the beach.
+
+            </h4>
+            <img src=""
+        </div>
+    );
+}
+
+export default Hobbies;
