b1e7daf5ce0f0b890f63eab43fd6e633ca711647
Brian <burbanci@emich.edu>
2021-10-03 00:03:07 +0000

    Updates to first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Hobbies from './Hobbies';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div>
+            <h2>
+                COSC 631: E Commerce
+            </h2>
+            <h3>
+                <Hobbies />
+            </h3>
+        </div>
+    )
 }
 
 export default App;
