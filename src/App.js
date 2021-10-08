93e2d9f769e43dc8b81f89816a60f71c94f0a740
Brian <burbanci@emich.edu>
2021-10-08 22:29:05 +0000

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
 import Education from './components/Education';
 import Hobbies from './components/Hobbies';
+import img from './images/pic.jpg';
 
 function App() {
     return (
       <div className='App'>
         <Navbar />
+        <img className='pic' src={img} alt='pic' />
         <div className='container'>
           <Education />
           <Hobbies />
