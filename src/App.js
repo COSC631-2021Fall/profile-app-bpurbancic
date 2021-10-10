4eeb3b06c0580328dc66ad9a45a31788d163fd56
Brian <burbanci@emich.edu>
2021-10-10 13:06:07 +0000

    Changes to App.js index.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     return (
       <div className='App'>
         <Navbar />
-        <img className='pic' src={img} alt='2-day-extension pup' width = "300" />
+        <img className='pic' src={img} alt='2-day-extension pup' width = "250" />
         <div className='container'>
           <Education />
           <Hobbies />
